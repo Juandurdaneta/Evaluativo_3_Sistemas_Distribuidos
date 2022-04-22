46a2a3643fa9e047a79201e58c32c9fcd287a50e
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2022-04-22 16:08:05 -0400

    containerized services

--- Auth_Service/app.js
+++ Auth_Service/app.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
+const host = process.env.HOST || '127.0.0.1';
 require('dotenv').config();
+
+const { Tracer, ExplicitContext, BatchRecorder, jsonEncoder } = require("zipkin");
+const { HttpLogger } = require("zipkin-transport-http");
+const zipkinMiddleware = require("zipkin-instrumentation-express").expressMiddleware;
+
+const ZIPKIN_ENDPOINT = process.env.ZIPKIN_ENDPOINT || "http://localhost:9411";
+
+// zipkin tracer
+const tracer = new Tracer({
+  ctxImpl: new ExplicitContext(),
+  recorder: new BatchRecorder({
+    logger: new HttpLogger({
+      endpoint: `${ZIPKIN_ENDPOINT}/api/v2/spans`,
+      jsonEncoder: jsonEncoder.JSON_V2,
+    }),
+  }),
+  localServiceName: "date-service",
+});
+
+
 // app config
 const app = express();
 app.use(express.json());
+
+app.use(zipkinMiddleware({ tracer }));
 
 // routes
 const userRoutes = require("./users/routes");
